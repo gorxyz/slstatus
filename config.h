94da0251be9578b7d6e9a9680896adb4538fa88f
sudurraaa <sudurrahah@protonmail.com>
2022-04-06 23:48:03 +0400

    ram used/ram_total

--- config.h
+++ config.h
@@ -65,7 +65,7 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function          format             argument */
-	{ ram_free,          "RAM %s/",     NULL },
+	{ ram_used,          "RAM %s/",     NULL },
 	{ ram_total,         "%s | ",       NULL },
 	{ cpu_perc,          "CPU %s%% | ",     NULL },
 	{ disk_free,         "/: %sB | ",      "/" },
